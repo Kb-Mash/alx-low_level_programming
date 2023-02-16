90b180c1f41284eb6f053eff3fc1848bb9cefc4c
Kb-Mash <karabo.kmashile@gmail.com>
2023-02-16 08:32:49 -0800

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - this program prints output to the standard error
- *
- * Return 1 (Success)
+ * main - Entry point
+ * Description: this program prints line to standard error
+ * Return: 1 (Success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
+
+
