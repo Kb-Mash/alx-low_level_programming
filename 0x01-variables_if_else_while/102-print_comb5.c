96c3ef0ceca603a6ae97f31d77a400d6a2427981
Kb-Mash <karabo.kmashile@gmail.com>
2023-02-17 13:50:46 -0800

    updated

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,20 +14,23 @@ int main(void)
 	{
 		for (j = 1; j <= 99; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-
-			if (i == 98 && j == 99)
+			if (i < j)
 			{
-				continue;
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+
+				if (i == 98 && j == 99)
+					continue;
+
+				putchar(',');
+				putchar(' ');
 			}
-			putchar(',');
-			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
