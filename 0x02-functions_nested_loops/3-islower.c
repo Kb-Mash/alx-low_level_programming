882453cd4383ee457bda0f1950689ad6e5001cbd
Kb-Mash <karabo.kmashile@gmail.com>
2023-02-21 05:33:08 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower -  checks for lowercase character
+ * @c: parameter for the argument
+ * Return: 1 (Success), 0 (fail)
+ */
+
+int _islower(int c);
+{
+	char cha = 'a';
+
+	while (cha <= 'z')
+	{
+		if (c == cha)
+			return (1);
+		cha++;
+	}
+
+	return (0);
+}
