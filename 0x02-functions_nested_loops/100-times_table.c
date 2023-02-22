34da0d2cee247b0853656402f5506d507035c833
Kb-Mash <karabo.kmashile@gmail.com>
2023-02-22 04:39:24 -0800

    updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,14 +36,10 @@ void print_times_table(int n)
 					_putchar(k + '0');
 				}
 				else
-				{
 					_putchar(k + '0');
-				}
 				if (j != n)
-				{
 					_putchar(',');
 					_putchar(' ');
-				}
 			}
 			_putchar('\n');
 			i++;
