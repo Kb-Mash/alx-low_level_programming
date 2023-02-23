5550b47f7bf2f18e6b4de8786fa7a93d6065740b
Kb-Mash <karabo.kmashile@gmail.com>
2023-02-23 13:49:30 -0800

    update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,18 +8,17 @@
 
 void print_line(int n)
 {
-	int i;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	else
+
+	int i = 1;
+
+	while (i <= n)
 	{
-		for (i = 1; i <= n; i++)
-		{
-			_putchar('_');
-		}
-		_putcher('\n');
+		_putchar('_');
+		i++;
 	}
+	_putchar('\n');
 }
