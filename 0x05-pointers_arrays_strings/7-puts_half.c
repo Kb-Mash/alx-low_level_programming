2f4e27292895b20b9453ba9995e9b7301e0b7ab6
Kb-Mash <karabo.kmashile@gmail.com>
2023-02-27 08:46:50 -0800

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: pointer for char parameter
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int len = 0, n;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (n % 2 != 0)
+	{
+		n = (len - 1) / 2;
+	}
+	else
+	{
+		n = len / 2;
+	}
+
+	while (n <= len)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
