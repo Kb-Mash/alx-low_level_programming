a6a20147ad75a838d15f5fb5676171e8624e5441
Kb-Mash <karabo.kmashile@gmail.com>
2023-02-27 09:37:42 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies a string
+ * @dest: A buffer to copy the string to
+ * @src: The string to copy
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
