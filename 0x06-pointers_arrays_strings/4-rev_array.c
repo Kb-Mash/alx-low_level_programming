84afd1e7654c2d52e35332decb223ceff8fde8d3
Kb-Mash <karabo.kmashile@gmail.com>
2023-03-01 15:05:43 -0800

    update

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, tmp, num = n - 1;
+	int i, tmp;
 
 	for (i = 0; i < n; i++)
 	{
 		tmp = a[i];
-		a[i] = a[num];
-		a[num] = tmp;
+		a[i] = a[n--];
+		a[n--] = tmp;
 	}
 }
