6dba3bbad45e9a7f4b553f7c97b98d359e9140b8
Kb-Mash <karabo.kmashile@gmail.com>
2023-03-01 13:39:13 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: returns an integer less than, equal to,
+ * or greater than zero if s1 is found,re‐spectively,
+ * to  be less than, to match, or be greater than s2.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 == *s2)
+		{
+			s1++, s2++;
+		}
+	}
+	return (*s1 - *s2);
+}
