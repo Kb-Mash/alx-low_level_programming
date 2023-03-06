aaafcf6e96ff6871ac39a4a6cb4e27b156bd5921
Kb-Mash <karabo.kmashile@gmail.com>
2023-03-06 11:19:56 -0800

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to be searched
+ * @accept: prefix to be measured
+ * Return: number of byte in @s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				n++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+			{
+				return (n);
+			}
+	}
+
+	s++;
+	}
+
+	return (n);
+}
