4366e7b801faa68e1eabd6861724f398ecd98f72
Kb-Mash <karabo.kmashile@gmail.com>
2023-03-08 11:41:36 -0800

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,14 +8,14 @@
 
 int factorial(int n)
 {
-	int result = n;
+	int fact = n;
 
-	if (n == 0 && n == 1)
+	if (n >= 0 && n <= 1)
 		return (1);
 
 	else if (n < 0)
 		return (-1);
-	result *= factorial(n - 1);
+	fact *= factorial(n - 1);
 
-	return (result);
+	return (fact);
 }
