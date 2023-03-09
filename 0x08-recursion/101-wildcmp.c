8c463ddd08b672a7123a4bb8b2c2060683155bce
Kb-Mash <karabo.kmashile@gmail.com>
2023-03-09 08:27:16 -0800

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,12 +19,12 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
 
-	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+	if (*s2 == '*')
 	{
-		return (1);
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
 
-	if (*s2 == '*' && *(s1 + 1) && *s2)
+	if (*s2 == '*' && *(s2 + 1) != '\0' && !*s1)
 	{
 		return (0);
 	}
