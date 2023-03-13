0a6b626200057d8e2780ca2ecd3419441045c81e
Kb-Mash <karabo.kmashile@gmail.com>
2023-03-13 03:46:23 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,44 +10,35 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	char *concat_str;
+	int i, index = 0, len = 0;
+
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
-	int i, j, len = 0, index = 0;
+	for (i = 0; s1[i]; i++)
+	{
+		len++;
+	}
 
 	for (i = 0; s2[i]; i++)
 	{
 		len++;
 	}
 
-	for (j = 0; s2[j]; j++)
-	{
-		len++;
-	}
-
-	char *concat_str = malloc(sizeof(char) * (len + 1));
+	concat_str = malloc(sizeof(char) * len);
 
 	if (concat_str == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; s1[i]; i++)
-	{
 		concat_str[index++] = s1[i];
-	}
 
-	for (j = 0; s2[j]; j++)
-	{
-		concat_str[index++] = s2[j];
-	}
+	for (i = 0; s2[i]; i++)
+		concat_str[index++] = s2[i];
 
 	return (concat_str);
 }
