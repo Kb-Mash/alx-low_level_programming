27cbbb6f24a622559048c4b28a795c4be6d44a76
Kb-Mash <karabo.kmashile@gmail.com>
2023-03-20 14:37:56 -0700

    requirement fail update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - a new type describing a dog
- * @name: name of dog
- * @age: age of the dog
- * @owner: owner of the dog
+/*
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog.
  */
 
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 struct dog
 {
 	char *name;
@@ -16,9 +20,8 @@ struct dog
 };
 
 /**
- * dog_t - typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
