3bbe1ef30ae44a2606654a358c345e097435a4e4
Kb-Mash <karabo.kmashile@gmail.com>
2023-03-24 12:29:35 -0700

    prints opcodes of its main

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mai - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0(success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
