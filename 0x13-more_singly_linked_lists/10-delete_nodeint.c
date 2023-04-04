eaa78e3a2851e5db8fcc56ab4782172290bfd3b7
Kb-Mash <karabo.kmashile@gmail.com>
2023-04-04 15:47:10 -0700

    while loop increment update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current = *head, *previous;
 	unsigned int i = 0;
 
-	if (head == NULL)
+	if (current == NULL)
 	{
 		return (-1);
 	}
@@ -34,10 +34,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 
 		current = current->next;
+		i++;
 	}
 
-	previous->next = current->next;
-	free(current);
+	previous = current->next;
+	current->next = previous->next;
+	free(previous);
 
 	return (1);
 }
