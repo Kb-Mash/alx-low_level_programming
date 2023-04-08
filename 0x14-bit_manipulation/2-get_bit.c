19195980baa204fc730e75451c018e220febe682
Kb-Mash <karabo.kmashile@gmail.com>
2023-04-08 13:33:12 -0700

    bitwise manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,16 +10,16 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if ((n & (1 << index)) == 0)
-	{
-		return (0);
-	}
-
 	int size = sizeof(n) * 8;
 
 	if (index >= size)
 	{
 		return (-1);
+	}
+
+	if ((n & (1 << index)) == 0)
+	{
+		return (0);
 	}
 
 	return (1);
