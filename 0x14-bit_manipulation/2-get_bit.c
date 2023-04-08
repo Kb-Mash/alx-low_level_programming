3b167cc24a03c619e5c63ce532a2d8c3cf9872e3
Kb-Mash <karabo.kmashile@gmail.com>
2023-04-08 12:24:06 -0700

    bitwise manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * get_bit -  returns the value of a bit at a given index
+ * @n: the decimal to get the bit from
+ * @index: the index, starting from 0 of the bit to get
+ * Return: the value of the bit at index
+ * or -1 if an error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if ((n & (1 << index)) == 0)
+	{
+		return (0);
+	}
+
+	int size = sizeof(n) * 8;
+
+	if (index >= size)
+	{
+		return (-1);
+	}
+
+	return (1);
+}
