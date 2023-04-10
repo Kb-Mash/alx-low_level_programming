6a13c6c17f93f99857ed83954fdecec6d557412e
Kb-Mash <karabo.kmashile@gmail.com>
2023-04-10 12:16:26 -0700

    header file update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,7 +13,5 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void _cp(const char *file_from, const char *file_to);
-void _close(int fd);
 
 #endif
