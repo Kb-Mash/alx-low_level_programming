3ea95328ef41edb071027e14cb94d6f09a565366
Kb-Mash <karabo.kmashile@gmail.com>
2023-05-08 05:48:54 -0700

    read and print text

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read from
+ * @letters: number of letters to read
+ * Return: actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	int fd;
+	size_t r, w;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	r = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (fd == -1 || r == -1 || w != r || w == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (w);
+}
