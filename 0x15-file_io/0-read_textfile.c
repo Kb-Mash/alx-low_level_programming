70b30658854dc941de0be4caeaf0c9b936ae69a8
Kb-Mash <karabo.kmashile@gmail.com>
2023-05-08 16:36:19 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,9 @@
 #include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
