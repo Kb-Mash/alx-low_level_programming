a1eaa2dc6600400c20987d06a8559a45129eefdb
Kb-Mash <karabo.kmashile@gmail.com>
2023-05-08 17:03:02 -0700

    read and print

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
@@ -11,18 +6,16 @@
  * @letters: number of letters to read
  * Return: actual number of letters it could read and print
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
-	int fd;
-	size_t r, w;
+	int fd, r, w;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	if (!buffer)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
