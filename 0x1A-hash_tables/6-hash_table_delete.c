dee0845096998f50d5815b2dc82621e3900a4fc7
Kb-Mash <karabo.kmashile@gmail.com>
2023-07-07 12:15:12 -0700

    update

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,7 +8,8 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_table_t *head = ht, *node = NULL, *tmp = NULL;
+	hash_node_t *node = NULL, *tmp = NULL;
+	hash_table_t *head = ht;
 
 	for (i = 0; i < ht->size; i++)
 	{
