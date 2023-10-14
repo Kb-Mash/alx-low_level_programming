0bf92240b2e7633333fd631409a27a0cc69a6d70
Kb-Mash <karabo.kmashile@gmail.com>
2023-10-14 04:42:37 -0700

    incorrect output

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -8,43 +8,33 @@
   * @value: The value to search for.
   *
   * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the first index where the value is located.
+  *         Otherwise, the first index where the value is located
   */
-
 int jump_search(int *array, size_t size, int value)
 {
-	int jump_point, low, high, i;
-	char *message;
+	int jump, prev, i, j = 0;
 
 	if (array == NULL)
 		return (-1);
 
-	jump_point = sqrt(size);
-	for (i = 0; i < (int)size; i += jump_point)
+	jump = sqrt(size);
+	for (i = 0, prev = 0; i < (int)size; i += jump)
 	{
-		if (value <= array[i])
-		{
-			low = i - 3;
-			high = i;
-			message = "Value found between indexes";
-			printf("%s [%d] and [%d]\n", message, low, high);
-			break;
-		}
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-	}
-	if (i > (int)size)
-	{
-		low = i - jump_point;
-		high = low;
-		message = "Value found between indexes";
-		printf("%s [%d] and [%d]\n", message, low, i);
-	}
-
-	for (i = low; i <= high; i++)
-	{
-		printf("Value checked array[%d] = [%d]\n", low, array[i]);
-		if (value == array[i])
+		if ((i + jump >= (int)size || array[i + jump] >= value) && !j)
+		{
+			prev = i;
+			i += jump;
+			printf("Value found between indexes [%d] and [%d]\n", prev, i);
+			if (i < (int)size)
+				size = i + 1;
+			i = prev - 1;
+			jump = 1;
+			j++;
+		}
+		else if (array[i] == value)
 			return (i);
+		prev = i;
 	}
 	return (-1);
 }
